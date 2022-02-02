9e2e4d8855f95063f16e2d9b2b0fd2f4ff22a842
Jenis700 <radadiyajenis2001@gmail.com>
2022-02-02 19:59:36 +0530

    firebase_simple_login

---
+++ lib/auth.dart
@@ -0,0 +1,50 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthenticationHelper {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // get user => _auth.currentUser;
+
+//SIGN UP METHOD
+  Future<String?> signUp(
+      {required String email, required String password}) async {
+    try {
+      await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return null;
+    } on FirebaseAuthException catch (e) {
+      return e.message;
+    }
+  }
+
+  // Future<String?> signUgp(
+  //     {required String email, required String password}) async {
+  //   try {
+  //     await _auth.sendSignInLinkToEmail(
+  //         email: email, actionCodeSettings: ActionCodeSettings);
+  //     return null;
+  //   } on FirebaseAuthException catch (e) {
+  //     return e.message;
+  //   }
+  // }
+
+  //SIGN IN METHODJ
+  Future<String?> signIn(
+      {required String email, required String password}) async {
+    try {
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+      return null;
+    } on FirebaseAuthException catch (e) {
+      return e.message;
+    }
+  }
+
+  //SIGN OUT METHOD
+  Future<void> signOut() async {
+    await _auth.signOut();
+
+    print('signout');
+  }
+}
